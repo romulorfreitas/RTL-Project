f2a054bba765284461c029bdabab594d0dfc4442
Rômulo Freitas <110496787+romulorfreitas@users.noreply.github.com>
2022-11-22 18:31:28 -0500

    Entrega exercicio 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Testa o componente NotFound', () => {
+  it('Testa se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const pageNotFound = screen.getByRole('heading', { name: /page requested not found/i });
+    const pikachuCrying = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    expect(pageNotFound && pikachuCrying).toBeInTheDocument();
+  });
+  it('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+    renderWithRouter(<NotFound />);
+
+    const pikachuCrying = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    const pikachuCryingLink = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const pikachuCryingAlt = 'Pikachu crying because the page requested was not found';
+
+    expect(pikachuCrying).toBeInTheDocument();
+    expect(pikachuCrying.src).toBe(pikachuCryingLink);
+    expect(pikachuCrying.alt).toBe(pikachuCryingAlt);
+  });
+});
